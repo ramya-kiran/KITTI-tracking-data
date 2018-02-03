09ccf2089bfe5ef2e89ffb3361e3a46be05adfb8
Ramya Rao <ramrao@umail.iu.edu>
2018-02-03 15:30:34 -0500

    minor changes

--- read_point_data.py
+++ read_point_data.py
@@ -1,4 +1,44 @@
+"""
+Script to convert images to tfrecords format
+usage: python images_to_tfrecords.py <list of images> -o <output>
+"""
+import tensorflow as tf
+import scipy.ndimage
+import numpy as np
 import argparse
+import os.path
+
+
+HEIGHT = 200
+WIDTH = 3
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+"""
+Read points into a matrix collectively
+"""
+def read_points(filenames):
+    points = np.zeros((len(filenames), HEIGHT, WIDTH), dtype=np.uint8)
+
+    for i,pt in enumerate(filenames):
+        print('Processing {} as class {}'.format(pt))
+        points[i] = np.loadtxt(pt)
+
+    return points
+
+
+def convert(filenames, output):
+    points = read_points(filenames)
+
+    print('Writing output to {}'.format(output))
+    with tf.python_io.TFRecordWriter(output) as writer:
+        for p in points:
+            raw = p.tostring()
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'point_raw': _bytes_feature(raw)
+            }))
+            writer.write(example.SerializeToString())
 
 def main():
     # Read arguments
@@ -9,8 +49,7 @@ def main():
     args = parser.parse_args()
 
     # Convert!
-    #convert(args.source, args.output)
-    print(len(args.source))
+    convert(args.source, args.output)
 
 if __name__ == '__main__':
     main()
